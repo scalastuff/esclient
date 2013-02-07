b6d7804f3193422081b01f46a0dfc20984108b61
Ruud Diterwich <ruud@diterwich.com>
2013-02-07 14:08:21 +0100

    Renamed apply to execute.

--- src/main/scala/org/scalastuff/esclient/Implicits.scala
+++
@@ -1,8 +0,0 @@
-package org.scalastuff.esclient
-
-import org.elasticsearch.client.Client
-
-package object implicits {
-  implicit def esclient(javaClient : Client) = 
-    new ESClient(javaClient)
-}
